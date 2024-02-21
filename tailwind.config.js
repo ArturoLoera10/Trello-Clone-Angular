f5a23555a281959c8bf3f14e619ae62c6b8afca6
Arturo Loera <arturo.loera@itsn.edu.mx>
2024-02-21 10:53:48 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        success: colors.green,
+        primary: colors.blue,
+      },
+      container: {
+        screens: {
+          mobile: "600px",
+          tablet: "900px",
+          desktop: "1200px",
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
